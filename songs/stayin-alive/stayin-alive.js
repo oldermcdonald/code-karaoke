2e73cd82f6cee6ae8c0d58bc44b06c5e59851068
Dave <oldmcdonald1@gmail.com>
2019-06-13 22:21:51 +1000

    stayin alive beegees added

---
+++ songs/stayin-alive/stayin-alive.js
@@ -0,0 +1,9 @@
+// Title: Stayin Alive
+// Artist: Bee Gees
+// Songwriters: Maurice Ernest Gibb / Robin Hugh Gibb / Barry Alan Gibb
+
+// infinite loop (stayin alive)
+let alive = true
+while (alive) {
+  console.log(`ha ha ha ha stayin alive`)
+}
